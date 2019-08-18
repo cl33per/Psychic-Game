9f0507256e3817600c0594d0c28bd349c749e0a6
cl33per <leeper08@gmail.com>
2019-08-17 22:04:10 -0400

    good stoping point. Functions work

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -11,44 +11,82 @@ var guesses = 10;
 // Create variables that hold references to the places in the HTML where we want to display things.
 var directionsText = document.getElementById("directions-text");
 var userChoiceText = document.getElementById("userchoice-text");
-// var computerChoiceText = document.getElementById("computerchoice-text");
+var userwonText = document.getElementById("userwon-text");
+var userlostText = document.getElementById("userlost-text");
+var computerChoiceText = document.getElementById("computerchoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesText = document.getElementById("guesses-text");
 
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-// This function is run whenever the user presses a key.
+   function resetcolors() {
+       document.getElementById("a").style.color = "#fff";
+       document.getElementById("b").style.color = "#fff";
+       document.getElementById("c").style.color = "#fff";
+       document.getElementById("d").style.color = "#fff";
+       document.getElementById("e").style.color = "#fff";
+       document.getElementById("f").style.color = "#fff";
+       document.getElementById("g").style.color = "#fff";
+       document.getElementById("h").style.color = "#fff";
+       document.getElementById("i").style.color = "#fff";
+       document.getElementById("j").style.color = "#fff";
+       document.getElementById("k").style.color = "#fff";
+       document.getElementById("l").style.color = "#fff";
+       document.getElementById("m").style.color = "#fff";
+       document.getElementById("n").style.color = "#fff";
+       document.getElementById("o").style.color = "#fff";
+       document.getElementById("p").style.color = "#fff";
+       document.getElementById("q").style.color = "#fff";
+       document.getElementById("r").style.color = "#fff";
+       document.getElementById("s").style.color = "#fff";
+       document.getElementById("t").style.color = "#fff";
+       document.getElementById("u").style.color = "#fff";
+       document.getElementById("v").style.color = "#fff";
+       document.getElementById("w").style.color = "#fff";
+       document.getElementById("x").style.color = "#fff";
+       document.getElementById("y").style.color = "#fff";
+       document.getElementById("z").style.color = "#fff";
 
+   }
+// This function is run whenever the user presses a key.
+ 
 document.onkeyup = function (event) {
 
     // Determines which key was pressed.
     var userGuess = event.key.toLowerCase();;
 
     // Randomly chooses a choice from the options array. This is the Computer's guess.
-    
+
     // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
     if (userGuess === computerGuess) {
         wins++;
+        userwonText.textContent = "Congrats you guessed the letter: " + computerGuess;
+        guesses = 10;
+        computerGuess = randomizeGuess();
+        resetcolors();
     } else {
         guesses--;
     }
     if (guesses === 0) {
         losses++
         guesses = 10;
-         computerGuess = randomizeGuess();
+        computerGuess = randomizeGuess();
+        resetcolors();
     }
-function randomizeGuess(computerGuess){
-    return computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-}
+
+    function randomizeGuess(computerGuess) {
+        return computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    }
 
 
     // Display the user and computer guesses, and wins/losses/ties.
     userChoiceText.textContent = "You chose: " + userGuess;
-    // computerChoiceText.textContent = "The computer chose: " + computerGuess;
+    computerChoiceText.textContent = "The computer chose: " + computerGuess;
     winsText.textContent = wins;
     lossesText.textContent = losses;
     guessesText.textContent = guesses;
+
 
     // Changes id property to hide useGuess letter
     document.getElementById(event.key).style.color = "#222";
